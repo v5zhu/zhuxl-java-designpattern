e5cd7e194fed550a11acfe6e4ce48677a68f02f1
v5zhu <v5zhu@qq.com>
2018-07-20 21:13:41 +0800

    适配器模式

---
+++ src/main/java/com/zhuxl/java/designpattern/$03adapter/AndroidInterface.java
@@ -0,0 +1,9 @@
+package com.zhuxl.java.designpattern.$03adapter;
+
+public interface AndroidInterface {
+
+    /**
+     * android的接口只能用android线充电
+     */
+    void chargeWithAndroidLine();
+}
---
+++ src/main/java/com/zhuxl/java/designpattern/$03adapter/classadapter/IphoneAdapter.java
@@ -0,0 +1,16 @@
+package com.zhuxl.java.designpattern.$03adapter.classadapter;
+
+import com.zhuxl.java.designpattern.$03adapter.AndroidLine;
+import com.zhuxl.java.designpattern.$03adapter.IphoneInterface;
+
+/**
+ * 创建一个苹果和android的转接头（适配器）,让苹果接口可以用android线来对接
+ */
+public class IphoneAdapter extends AndroidLine implements IphoneInterface {
+
+    @Override
+    public void chargeWithIphoneLine() {
+        System.out.print("使用转接头Iphone转Android->");
+        chargeWithAndroidLine();
+    }
+}
